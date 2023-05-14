bb203c9f64b4002329da95a2a173288d9562a16b
Ty <phuongpham20012001@gmail.com>
2023-05-14 03:41:23 +0200

    change order css for user

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -24,43 +24,45 @@ const Order = () => {
   }
 
   return (
-    <table className={styles.table}>
-      <thead>
-        <tr>
-          <th className={styles.th}>Total Amount</th>
-          <th className={styles.th}>Items</th>
-          <th className={styles.th}>Status</th>
-        </tr>
-      </thead>
-      <tbody>
-        {orders.map((order) => {
-          const key = `${order._id.customerId}-${order._id.totalAmount}-${order._id.status}`;
-          return (
-            <tr key={key}>
-              <td className={styles.td}>${order._id.totalAmount.toFixed(2)}</td>
-              <td className={styles.td}>
-                <ul className={styles.ul}>
-                  {order.items.map((item, index) => (
-                    <li key={index} className={styles.li}>
-                      <div className={styles.itemContainer}>
-                        <img src={item.image} alt={item.name} className={styles.itemImage} />
-                        <div className={styles.itemDetails}>
-                          <div className={styles.itemName}>{item.name}</div>
-                          <div className={styles.itemQuantity}>Quantity: {item.quantity}</div>
+    <div className={styles.tableContainer}>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <th className={styles.th}>Total Amount</th>
+            <th className={styles.th}>Items</th>
+            <th className={styles.th}>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {orders.map((order) => {
+            const key = `${order._id.customerId}-${order._id.totalAmount}-${order._id.status}`;
+            return (
+              <tr key={key}>
+                <td className={styles.td}>${order._id.totalAmount.toFixed(2)}</td>
+                <td className={styles.td}>
+                  <ul className={styles.ul}>
+                    {order.items.map((item, index) => (
+                      <li key={index} className={styles.li}>
+                        <div className={styles.itemContainer}>
+                          <img src={item.image} alt={item.name} className={styles.itemImage} />
+                          <div className={styles.itemDetails}>
+                            <div className={styles.itemName}>{item.name}</div>
+                            <div className={styles.itemQuantity}>Quantity: {item.quantity}</div>
+                          </div>
                         </div>
-                      </div>
-                    </li>
-                  ))}
-                </ul>
-              </td>
-              <td className={`${styles.td} ${styles[order._id.status.toLowerCase()]}`}>
-                {order._id.status}
-              </td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+                      </li>
+                    ))}
+                  </ul>
+                </td>
+                <td className={`${styles.td} ${styles[order._id.status.toLowerCase()]}`}>
+                  {order._id.status}
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
